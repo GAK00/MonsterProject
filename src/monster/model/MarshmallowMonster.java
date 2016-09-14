367115bf9016d8a16c50b1ebba59ded7318f6ba2
GAK00 <geran.kunz@gmail.com>
2016-09-14 09:52:59 -0600

    fixed formatting

--- src/monster/model/MarshmallowMonster.java
+++ src/monster/model/MarshmallowMonster.java
@@ -1,4 +1,3 @@
-
 package monster.model;
 
 public class MarshmallowMonster
@@ -9,15 +8,19 @@ public class MarshmallowMonster
 	private boolean hasBellyButton;
 	private int noseCount;
 	private int armCount;
-	public MarshmallowMonster(){
+
+	public MarshmallowMonster()
+	{
 		this.name = "no name here";
 		this.antennaCount = 0;
-		this.eyeCount= 0;
+		this.eyeCount = 0;
 		this.hasBellyButton = false;
 		this.noseCount = 0;
 		this.armCount = 0;
 	}
-	public MarshmallowMonster(String name, double antennaCount,int eyeCount,boolean hasBellyButton,int noseCount,int armCount){
+
+	public MarshmallowMonster(String name, double antennaCount, int eyeCount, boolean hasBellyButton, int noseCount, int armCount)
+	{
 		this.name = name;
 		this.antennaCount = antennaCount;
 		this.eyeCount = eyeCount;
@@ -25,54 +28,82 @@ public class MarshmallowMonster
 		this.noseCount = noseCount;
 		this.armCount = armCount;
 		/*
-		 *name = Zambumafu The destroyer
-		 *4 antenna 2 .5 and 2 .25
-		 *2 eyes
-		 *hasBellyButton = false;
-		 *nose count 1
-		 *armcount = 2
-		 *
+		 * name = Zambumafu The destroyer4 antenna 2 .5 and 2 .252 eyes
+		 * hasBellyButton = false;nose count 1armcount = 2
 		 */
 	}
-public String getName(){
-	return name;}
-public double getAntennaCount(){
-	return antennaCount;
-}
-public int getEyeCount(){
-	return eyeCount;
-}
-public boolean getHasBellyButton(){
-	return hasBellyButton;
-	}
-public int getNoseCount(){
-	return noseCount;
-}
-public int getArmCount(){
-	return armCount;
-}
-public void setName(String name){
-	this.name = name;}
-public void setAntennaCount(double antennaCount){
-	this.antennaCount = antennaCount;
-}
-public void setEyeCount(int eyeCount){
-	this.eyeCount = eyeCount;
-}
-public boolean setHasBellyButton(){
-	return hasBellyButton;
-	}
-public void setNoseCount(int noseCount){
-	this.noseCount = noseCount;
-}
-public void setArmCount(int armCount){
-	this.armCount = armCount;
-}
-public String toString(){
-	String bellyButton;
-	if(hasBellyButton){bellyButton = "has a belly button, ";}
-	else{bellyButton = "does not have a belly button, ";}
-	return(name+" has "+antennaCount+" antennas , " + eyeCount+" eyes , " + bellyButton + armCount +" arms and " +noseCount+" noses");
 
-}
+	public String getName()
+	{
+		return name;
+	}
+
+	public double getAntennaCount()
+	{
+		return antennaCount;
+	}
+
+	public int getEyeCount()
+	{
+		return eyeCount;
+	}
+
+	public boolean getHasBellyButton()
+	{
+		return hasBellyButton;
+	}
+
+	public int getNoseCount()
+	{
+		return noseCount;
+	}
+
+	public int getArmCount()
+	{
+		return armCount;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public void setAntennaCount(double antennaCount)
+	{
+		this.antennaCount = antennaCount;
+	}
+
+	public void setEyeCount(int eyeCount)
+	{
+		this.eyeCount = eyeCount;
+	}
+
+	public boolean setHasBellyButton()
+	{
+		return hasBellyButton;
+	}
+
+	public void setNoseCount(int noseCount)
+	{
+		this.noseCount = noseCount;
+	}
+
+	public void setArmCount(int armCount)
+	{
+		this.armCount = armCount;
+	}
+
+	public String toString()
+	{
+		String bellyButton;
+		if (hasBellyButton)
+		{
+			bellyButton = "has a belly button, ";
+		} else
+		{
+			bellyButton = "does not have a belly button, ";
+		}
+		return (name + " has " + antennaCount + " antennas , " + eyeCount + " eyes , " + bellyButton + armCount + " arms and " + noseCount + " noses");
+
+	}
 }
